b442ac8ef381acb152a4e3e533d6f3736c00c8f3
a.chrzaszcz <a.chrzaszcz@onebid.group>
2021-10-29 22:49:35 +0200

    zmiana wygladu partyPreview

--- src/components/PartyPreview.js
+++ src/components/PartyPreview.js
@@ -1,6 +1,8 @@
 import '../styles/NearbyParties.css';
+import {FiChevronRight, FiCircle} from "react-icons/fi";
+import React from "react";
 
-export default function PartyPreview({buttonText, index, channels}) {
+export default function PartyPreview({buttonText, index, channels, myParty, endTime}) {
 
     const getDelay = () => {
         return (index * 0.2) + 's';
@@ -9,7 +11,7 @@ export default function PartyPreview({buttonText, index, channels}) {
     const activeListeners = () => {
         let activeListeners = 0;
         channels.forEach(channel => {
-            activeListeners += channel.activeListeners.length;
+            activeListeners += channel.activeListeners?.length;
         })
         return activeListeners;
     }
@@ -33,10 +35,16 @@ export default function PartyPreview({buttonText, index, channels}) {
 
     return (
         <div style={myPartyPreview}>
+            {myParty &&
+            <FiCircle style={{position: 'absolute', right: '3%', top: '5%'}} color={endTime ? 'red' : 'green'} fill={endTime ? 'red' : 'green'} size='7%'/>
+            }
             <text style={{color: 'white', fontSize: '4vh', margin: '4% 0'}}>{buttonText}</text>
             <text className='x' style={{color: 'white', fontSize: '2.5vh'}}>Bar Zamieszanie</text>
-            <text className='x' style={{color: 'white', fontSize: '2.5vh'}}>{activeListeners()} users</text>
-            <text className='join' style={{color: 'white', fontSize: '4vh', textAlign: 'center', margin: '5% 0'}}>Join now!</text>
+            <text className='x' style={{color: 'white', fontSize: '2.5vh'}}>{activeListeners() === 1 ? '1 user' : activeListeners() + ' users'}</text>
+            <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', margin: '5% 0'}}>
+                <text className='join' style={{color: 'white', fontSize: '4vh', textAlign: 'center'}}>{myParty ? 'See details' : 'Join now!'}</text>
+                <FiChevronRight color='white' size='9%' style={{margin: '0 4%'}}/>
+            </div>
         </div>
     );
 }
